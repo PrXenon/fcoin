35cce1404e8822052228fb51c95213b98a4fee83
Jonas Eisenmann <developer@prxenon.com>
2013-03-31 00:39:25 +0100

    Update 1.8.6

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
 fcoin
 =====
 
-Faircoin ("fair coin")
-
-Fcoin is a complementary, non-profit money system.
-It’s interest free and has a little negative interest.
-Fcoins are created simply by exchanging them.
-The value is 1 working hour = 10 fcoins.
-Users have to trust to each other and therefore give guarantees.
-
-The user database and functions work at http://www.fcoin.de
-The Fcoin App gives easy access from Android.
-It only needs internet access rights.
-
-Although the origin of fcoin is germany,
-you can use it anywhere in the world and open trading circles with fcoin.
-Feel free to contact us for support or additional
-administrative privileges if you want to start a new circle.
+Faircoin ("fair coin") is a free, non-profit, independent, interest-free and 100% power Tausch-/Geldwährung covered.  The aim of fcoin, is offering a currency that is not output as conventional banks, but of each and every of us.
--- src/com/f/coin/Marktplatz.java
+++ src/com/f/coin/Marktplatz.java
@@ -1,10 +1,16 @@
 package com.f.coin;
 
+import java.net.URL;
+
+import android.net.MailTo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
@@ -30,6 +36,8 @@ public class Marktplatz extends Activity {
 	            Window.PROGRESS_VISIBILITY_ON);
 		
 		
+	 
+		
 		WebViewClient marktwebnew = new WebViewClient()
 	       {
 	           
@@ -42,15 +50,64 @@ public class Marktplatz extends Activity {
 
 			// Override page so it's load on my view only
 	           @Override
+	           
 	           public boolean shouldOverrideUrlLoading(WebView view, String  url)
+	           
 	           {
-	        	   view.loadUrl(url);
 	        	   
-	            // Return true to override url loading (In this case do nothing).
-	            return true;
-	           }
-	       };
-	       
+	        	   if (url.indexOf("tel:") > -1) {
+	   	            startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(url)));
+	   	            return true;
+	        	   }
+	        	   if(url.startsWith("mailto:")){
+	        		   String address = url.replace("mailto:", "");
+	        		   MailTo mt = MailTo.parse(url);
+	                   Intent i = newEmailIntent(Marktplatz.this, address, mt.getSubject(), mt.getBody(), mt.getCc());
+	                   startActivity(i);
+	                   view.reload();
+	                   return true;
+	               }
+	        	   if(url.startsWith("share:")){
+	        		   String address = url.replace("share://fcoin.prxenon.com/mmarket/detail.php?aid=", "fcoin.prxenon.com/index.php?q=detail&id=");
+	        		   
+	        		   Intent intent = new Intent(Intent.ACTION_SEND);
+	        		   intent.setType("text/plain");
+	        		   intent.putExtra(Intent.EXTRA_TEXT, "Ich empfehle: http://"+ address);
+	        		   startActivity(Intent.createChooser(intent, "Teilen über"));
+	                   return true;
+	               }
+	        	   try {
+	   	        	URL urlObj = new URL(url);
+	   	         if( TextUtils.equals(urlObj.getHost(),"fcoin.prxenon.com/index.php") ) {
+	   	        	
+ 					Intent markt = new Intent(Marktplatz.this, Marktplatz.class);
+ 					Marktplatz.this.finish();
+ 		            Marktplatz.this.startActivity(markt);
+ 		            
+     		      } 
+	   	      
+	   	   
+	   	      if( TextUtils.equals(urlObj.getHost(),"fcoin.prxenon.com") ) {
+	   	        	
+		   	         view.loadUrl(url);
+		   	      
+		            
+   		      } 
+	   	        else {
+	   	        	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+	   	        	startActivity(intent);
+	   	      return true;
+  		      }
+  		    }
+  		    catch (Exception e) {
+  		      e.printStackTrace();
+  		    }
+		return true;
+     }
+ };
+ 
+ 
+ 
 	       final ProgressDialog progressDialog = new ProgressDialog(activity);
 	       progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
 	       progressDialog.setMessage("Daten werden geladen...");
@@ -83,7 +140,15 @@ public class Marktplatz extends Activity {
 	    
 	}
 	
-	
+	public static Intent newEmailIntent(Context context, String address, String subject, String body, String cc) {
+	      Intent intent = new Intent(Intent.ACTION_SEND);
+	      intent.putExtra(Intent.EXTRA_EMAIL, new String[] { address });
+	      intent.putExtra(Intent.EXTRA_TEXT, body);
+	      intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+	      intent.putExtra(Intent.EXTRA_CC, cc);
+	      intent.setType("message/rfc822");
+	      return intent;
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
